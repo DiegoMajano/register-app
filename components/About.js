70eddba75a72d257d2171b0d07a74f99888a3e9f
DiegoMajano <dmajano@gmail.com>
2025-08-25 11:47:26 -0600

    adding descripcion

--- components/About.js
+++ components/About.js
@@ -12,16 +12,23 @@ export default function About() {
                 <Text className="text-xl font-semibold text-purple-700 mb-3">Desarrollador</Text>
 
                 <View className="space-y-1">
-                    <Text className="text-gray-800 font-medium">Nombre:
+                    <Text className="text-gray-800 font-medium mb-3">Nombre:
                         <Text className="text-gray-600"> Diego José Rodríguez Majano</Text>
                     </Text>
-                    <Text className="text-gray-800 font-medium">Carnet:
+                    <Text className="text-gray-800 font-medium mb-3">Carnet:
                         <Text className="text-gray-600"> RM220481</Text>
+                    </Text>
+                    <Text className="text-gray-800 font-medium mb-3">Correo:
+                        <Text className="text-gray-600"> diego.majano14@gmail.com</Text>
+                    </Text>
+                    <Text className="text-gray-800 font-medium mb-3">Descripción:
+                        <Text className="text-gray-600">Estudiante de 4to año de Ingeniería en Ciencias de la Computación, actualmente con habilidades de desarrollo fullstack con diferentes tecnologías, en FrontEnd React con JS y TS, VueJS, en BackEnd ExpressJs, Laravel y .NET, en bases de datos sólidos conocimientos en SQL y NoSQL, con MySQL, mongoDB y Firebase. Actualemnte formo parte de equipo de desarrollo para webapps y requerimientos que sean necesarios, y proximamente participare en el desarrollo de una app implementando React Native.
+                        </Text>
                     </Text>
                 </View>
 
                 <Text className="text-gray-700 mt-4 leading-relaxed">
-                    Descripción: Ejercicio de clase con
+                    Descripción de la app: Ejercicio de clase con
                     <Text className="font-semibold text-purple-600"> React Navigation</Text> +
                     <Text className="font-semibold text-purple-600"> AsyncStorage</Text>.
                     Como extra, integré <Text className="font-semibold text-purple-600">NativeWind</Text> para usar estilos con TailwindCSS.
